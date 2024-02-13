c299392103ab3e87fd18cad8b8ce6a44f02d336e
antonio <a.s.panferov@mail.ru>
2024-02-13 21:28:33 +0300

    download microservice

---
+++ home9/serverStore/src/test/java/ru/antonio/serverStore/ServerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.antonio.serverStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
